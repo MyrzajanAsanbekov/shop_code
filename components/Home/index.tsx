5ea7de4b1237ca6347b0d3d5d391d7d102394c64
asanbekovmyrzajan1@gmail.com <asanbekovmyrzajan1@gmail.com>
2024-09-15 10:35:13 +0600

    header UI

---
+++ components/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div id='home'>
+            <div className="container">
+                <div className="home">
+                    <h1>wadks,mndwekjds,m</h1>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Home;
